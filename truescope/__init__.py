76b8ed384a40f6e367016e8d2323986fbdd1e665
reazs <reaz.shakil@onegoalgraduation.org>
2022-08-22 16:07:49 -0400

    flask_truescope 1.00.00

--- truescope/__init__.py
+++ truescope/__init__.py
@@ -8,8 +8,8 @@ from flask_admin import Admin
 app = Flask(__name__)
 
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tztkrkcgvulyve:3e9432baa140f37bd2702a7655e72dc0b9a1fd302d56147b71bed445ac252274@ec2-34-199-68-114.compute-1.amazonaws.com:5432/d9bt31216sb5kl'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tztkrkcgvulyve:3e9432baa140f37bd2702a7655e72dc0b9a1fd302d56147b71bed445ac252274@ec2-34-199-68-114.compute-1.amazonaws.com:5432/d9bt31216sb5kl'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "sadjfkadfa09234"
 db = SQLAlchemy(app)
