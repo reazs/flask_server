e86df47bec2c7dae0cea4dc47f2414260cd802a8
reazs <reaz.shakil@onegoalgraduation.org>
2022-08-23 07:42:57 -0400

    tweaked app for heroku

--- truescope/__init__.py
+++ truescope/__init__.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 CORS(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tztkrkcgvulyve:3e9432baa140f37bd2702a7655e72dc0b9a1fd302d56147b71bed445ac252274@ec2-34-199-68-114.compute-1.amazonaws.com:5432/d9bt31216sb5kl'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qzhcvvkcvrcboz:a1ad05e1d1675a07058d26951da68e0109d964ffc2ae5f8cc9e77a1a1df671e0@ec2-'
+'44-193-178-122.compute-1.amazonaws.com:5432/d1fdu8dfhb60v4'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "sadjfkadfa09234"
 db = SQLAlchemy(app)
