c83dac2b12e84236692084c6f15e85f77e484af9
reazs <reaz.shakil@onegoalgraduation.org>
2022-09-05 14:40:55 -0400

    make it better

--- truescope/__init__.py
+++ truescope/__init__.py
@@ -9,7 +9,8 @@ from flask_msearch import Search
 app = Flask(__name__)
 
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['postgres://tcezfvpubcnejt:adbd753a64fdf7f3797530f681f7da05b4e9354e30fd231382c9bd3eb1573cc3@ec2-44-199-22-207.compute-1.amazonaws.com:5432/d4jum54hsrqhfaPS F:\web_development\flask_ptyhon\truescope\flask_server']= 'sqlite:///data.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qzhcvvkcvrcboz:a1ad05e1d1675a07058d26951da68e0109d964ffc2ae5f8cc9e77a1a1df671e0@ec2-'
 '44-193-178-122.compute-1.amazonaws.com:5432/d1fdu8dfhb60v4'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
